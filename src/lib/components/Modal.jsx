4641afff68c2120722da3be8b263c632dc2a24ca
Stephen Thomas <stepheniki17@gmail.com>
2023-09-12 15:48:56 +0200

    ok

---
+++ src/lib/components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './lib/Modal.css';
+
+export function Modal({ isOpen, content, onClose }) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal-background" onClick={onClose}>
+      {/* Contenu de la modal */}
+      <div className="modal-content">
+        <h2>{content}</h2>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
